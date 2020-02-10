81fb1077ac3c52a53aeca10502d659f85aa16ec6
Emanuell Amorim <amorimnuell@gmail.com>
2020-02-10 19:51:11 -0400

    Create project
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Source/SnakeClone/SnakeCloneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SnakeCloneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAKECLONE_API ASnakeCloneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
